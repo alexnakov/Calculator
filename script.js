4f991ebfef874f4da43d672c923b5bec4748e023
Alex Nakov <alex.nakov3@gmail.com>
2021-10-09 16:23:05 +0100

    keypad adds chars to the display and expression is evaluated when the = key is pressed

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-function mF() {
-  document.getElementById('display').innerHTML = "This is spot on!";
+function one() {
+  document.getElementById('display').innerHTML += "1";
+}
+function two() {
+  document.getElementById('display').innerHTML += "2";
+}
+function three() {
+  document.getElementById('display').innerHTML += "3";
+}
+function four() {
+  document.getElementById('display').innerHTML += "4";
+}
+function five() {
+  document.getElementById('display').innerHTML += "5";
+}
+function six() {
+  document.getElementById('display').innerHTML += "6";
+}
+function seven() {
+  document.getElementById('display').innerHTML += "7";
+}
+function eight() {
+  document.getElementById('display').innerHTML += "8";
+}
+function nine() {
+  document.getElementById('display').innerHTML += "9";
+}
+function add() {
+  document.getElementById('display').innerHTML += "+";
+}
+function minus() {
+  document.getElementById('display').innerHTML += "-";
+}
+function multi() {
+  document.getElementById('display').innerHTML += "*";
+}
+function divide() {
+  document.getElementById('display').innerHTML += "/";
+}
+function zero() {
+  document.getElementById('display').innerHTML += "0";
+}
+function wipe() {
+  document.getElementById('display').innerHTML = "";
+}
+function workout() {
+  expression = document.getElementById('display').innerHTML;
+  result = eval(expression);
+  document.getElementById('display').innerHTML = result;
 }
